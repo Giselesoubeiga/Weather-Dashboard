fa20855c0fc35d46a3c0585c188024b1d437279a
Gisele <giskey11@gmail.com>
2020-01-17 13:26:25 -0500

    add first js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function(){
+    https://openweathermap
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID=0946b5eb988b3caf2e24954f8caf2636"
+    $('#btn-ajax').click(function(){
+        $.get(url,function(data,status){
+            console.log(data)
+      })
+    })
+        
+})
