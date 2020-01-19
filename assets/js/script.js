94f4645ae1e01cee436c1497dcc9ae4e3ed8d302
Gisele <giskey11@gmail.com>
2020-01-19 17:31:21 -0500

    set the city and the date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
 
 $(document).ready(function(){
-    https://openweathermap
+
+
     var url = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID=0946b5eb988b3caf2e24954f8caf2636"
-    $('#btn-ajax').click(function(){
+   
         $.get(url,function(data,status){
-            console.log(data)
+            console.log(data.name)
+            
+            var currentDate = new Date(data.dt * 1000).toISOString(); //https://stackoverflow.com/questions/56070796/show-day-name-instead-of-number-from-open-weather-api-response
+
+             var display =  data.name + " (" + moment(currentDate).format("MM/D/YYYY") + ")"
+            $('#cityName').text(display )
       })
-    })
+
+
+   
         
 })
